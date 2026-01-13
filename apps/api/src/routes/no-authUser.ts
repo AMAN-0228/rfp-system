f5369b7fc15db664b23eb3a11f1f13ece6fe510c
aman <amansahu5051@gmail.com>
2026-01-13 23:38:53 +0530

    Add: routes forgot pass, reset pass and the there controller

--- apps/api/src/routes/no-authUser.ts
+++ apps/api/src/routes/no-authUser.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express';
 import { asyncHandler } from '../middleware/asyncHandler';
-import { registerUser, verifyOtpForRegistrationForUser, loginUser } from '../controllers/userAuth';
+import { registerUser, verifyOtpForRegistrationForUser, loginUser, forgotPasswordForUser, forgotPasswordVerifyOtpForUser, resetPasswordForUser } from '../controllers/userAuth';
 
 const router = Router();
 
@@ -9,5 +9,8 @@ router.post('/register', asyncHandler(registerUser));
 router.post('/verify-otp-for-registration', asyncHandler(verifyOtpForRegistrationForUser));
 
 router.post('/login', asyncHandler(loginUser));
+router.post('/forgot-password', asyncHandler(forgotPasswordForUser));
+router.post('/forgot-password-verify-otp', asyncHandler(forgotPasswordVerifyOtpForUser));
+router.post('/reset-password', asyncHandler(resetPasswordForUser));
 
 export default router;
--- apps/api/src/utils/password.ts
+++ apps/api/src/utils/password.ts
@@ -1,10 +1,14 @@
 
 import bcrypt from 'bcrypt';
 import { env } from '../config/env';
+import prisma from '../config/database';
+import { TokenPayload } from './tokens';
+import { sendOtp, verifyOtp } from './opt';
+import { ValidationError } from './errors';
 
 export const hashPassword = async (password: string): Promise<string> => {
   if (!password) {
-    throw new Error('Password is required for hashing');
+    throw new ValidationError('Password is required for hashing');
   }
 
   const hashedPassword = await bcrypt.hash(password, env.SALT_ROUNDS);
@@ -18,13 +22,72 @@ export const verifyPassword = async (
 ): Promise<boolean> => {
   // Validate inputs
   if (!plainPassword) {
-    throw new Error('Plain password is required for verification');
+    throw new ValidationError('Plain password is required for verification');
   }
   if (!hashedPassword) {
-    throw new Error('Hashed password is required for verification');
+    throw new ValidationError('Hashed password is required for verification');
   }
 
   const isMatch = await bcrypt.compare(plainPassword, hashedPassword);
 
   return isMatch;
+};
+
+const changePassword = async (userId: number, password: string) => {
+  if (!userId || !password) {
+    throw new ValidationError('User ID and password are required for changing password');
+  }
+  const hashedPassword = await hashPassword(password);
+  await prisma.users.update({
+    where: { id: userId },
+    data: { password: hashedPassword },
+  });
+};
+
+export const resetPassword = async ({ email, password, oldPassword, isForgotPassword }: { email: string, password: string, oldPassword?: string, isForgotPassword: boolean }, auth?: TokenPayload) => {
+  if (!email || !password) {
+    throw new ValidationError('Email and new password are required for resetting password');
+  }
+  if (!isForgotPassword && !oldPassword) {
+    throw new ValidationError('Old password is required for resetting password');
+  }
+  const user = await prisma.users.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (!user) {
+    throw new ValidationError('User not found');
+  }
+
+  if (!isForgotPassword) {
+    const isMatch = await verifyPassword(oldPassword!, user.password);
+    if (!isMatch) {
+      throw new ValidationError('Old password is incorrect');
+    }
+  }
+  return changePassword(user.id, password);
+};
+
+export const forgotPassword = async (email: string) => {
+  if (!email) {
+    throw new ValidationError('Email is required for forgot password');
+  }
+  const user = await prisma.users.findUnique({
+    where: {
+      email,
+    },
+  });
+  if (!user) {
+    throw new ValidationError('User not found');
+  }
+  await sendOtp(email);
+  // return true;
+};
+
+export const forgotPasswordVerify = async (email: string, otp: string) => {
+if (!email || !otp) {
+    throw new ValidationError('Email and OTP are required for forgot password verification');
+  }
+  return await verifyOtp({ email, otp });
 };
